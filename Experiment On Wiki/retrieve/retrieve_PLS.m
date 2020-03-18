8f03da2648c1e2122e1214bf7bdd065c61b5f75b
TongJiayan <>
2020-03-18 22:02:05 +0800

    Adjust structure of project

--- Experiment On Wiki/retrieve/retrieve_PLS.m
+++
@@ -1,9 +0,0 @@
-function [result_list] = retrieve_PLS(type,testData,BETA)
-    text_feature = [ones(size(testData.text,1),1) testData.text]*BETA;
-    if strcmp(type, 'text-to-image')
-        dist = pdist2(single(text_feature), single(testData.image), 'cosine');
-    elseif strcmp(type, 'image-to-text')
-        dist = pdist2(single(testData.image), single(text_feature), 'cosine');
-    end
-    [~,result_list] = sort(dist,2,'ascend');
-end
