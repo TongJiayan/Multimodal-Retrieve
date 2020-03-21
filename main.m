1599183c6587a25d1e37947996cd2c8d248c9d71
TongJiayan <>
2020-03-21 16:32:04 +0800

    Optimize parameters and add data pre-processing

--- main.m
+++ main.m
@@ -6,38 +6,48 @@ config = initialize();
 clear root_path;
 %%
 % LOAD TRAIN DATA
-if strcmp(config.general.dataset,'PASCAL')
-    data = load_pascal(config);  
-elseif strcmp(config.general.dataset,'WIKI')
+dataset = config.general.dataset;
+if strcmp(dataset,'PASCAL')
+    data = load_pascal(config); 
+elseif strcmp(dataset,'WIKI')
     data = load_wiki(config);
 end
 
-%%
 % TRAIN MODEL and TEST
 dataCell = cell(2,1);
 dataCell{1,1}.label = (1:1:config.trainDataSize).';
 dataCell{2,1}.label = (1:1:config.trainDataSize).';
 dataCell{1,1}.data = data.train.text;
 dataCell{2,1}.data = data.train.image;
+%%
 
 if strcmp('CCA',config.general.algorithm)   
 %    [A,B,r,U,V] = canoncorr(F.gist,F.wc);
 %    result_list = retrieve_CCA('image-tag',TF,A,B);
-%    disp('Relative images have been retrieved according to tag feature.');   
+%    disp('Relative images have been retrieved according to tag feature.');  
     options.method='CCA';
-    options.Factor= 13;
-    options.Lamda = 1;
     options.ReguAlpha =0.01;
+    if strcmp(dataset,'PASCAL')
+        options.Factor= 13;
+        options.Lamda = 1;  
+    elseif strcmp(dataset,'WIKI')
+        options.Factor= 13;
+        options.Lamda = 1;  
+    end
 elseif strcmp('PLS',config.general.algorithm)   
 %     [XL,YL,XS,YS,BETA,PCTVAR,MSE] = plsregress(F.wc,F.gist,16);
 %     result_list = retrieve_PLS('image-tag',TF,BETA);
 %     disp('Relative images have been retrieved according to tag feature.');
 
     options.method='PLS';
-    options.Factor= 14; %15
-    options.Lamda = 10; %10
     options.ReguAlpha =0.01;
-    
+    if strcmp(dataset,'PASCAL')
+        options.Factor= 63;
+        options.Lamda = 1;  
+    elseif strcmp(dataset,'WIKI')
+        options.Factor= 14;
+        options.Lamda = 1;  
+    end
     % ncomp = find(cumsum(PCTVAR(2,:)) >= pve,1, 'first');
     % plot(1:350,cumsum(100*PCTVAR(2,:)));
     % plot(1:350,100*PCTVAR(2,:));
@@ -45,16 +55,26 @@ elseif strcmp('PLS',config.general.algorithm)
     % ylabel('Percent Variance Explained in y');  
 elseif strcmp('BLM',config.general.algorithm) 
     options.method = 'BLM';
-    options.Factor= 61; %9
-    options.Lamda = 200;
+    if strcmp(dataset,'PASCAL')
+        options.Factor= 61;
+        options.Lamda = 200;  
+    elseif strcmp(dataset,'WIKI')
+        options.Factor= 13;
+        options.Lamda = 2;  
+    end
 elseif strcmp('GMMFA',config.general.algorithm)  
     options.method = 'MFA';
-    options.Factor= 57;
-    options.Lamda = 10; 
-    options.Mult = [1 1];
     options.ReguAlpha = 1e-6;
+    options.Mult = [1 1];
+    if strcmp(dataset,'PASCAL')
+        options.Factor= 57;
+        options.Lamda = 10;  
+    elseif strcmp(dataset,'WIKI')
+        options.Factor= 6;
+        options.Lamda = 1;  
+    end    
 end
-%%
+
 Wout = Newgma(dataCell,options);    
 result_list = retrieve(config.general.direction, data.test, Wout);
 
--- util/load_wiki.m
+++ util/load_wiki.m
@@ -12,11 +12,19 @@ function [data] = load_wiki(config)
     data.train = {};
     data.test = {};
     load(dataPath);
-    data.train.image = I_tr;
-    data.train.text = T_tr;
+    data.train.image = pre_process(I_tr);
+    data.train.text = pre_process(T_tr);
     data.train.cat_list = trainCatList.data;
-    data.test.image = I_te;
-    data.test.text = T_te;
+    data.test.image = pre_process(I_te);
+    data.test.text = pre_process(T_te);
     data.test.cat_list = testCatList.data;
 end
 
+function[processed_data] = pre_process(raw_data)
+    dataSize = size(raw_data,1);
+    % Decentralization
+    processed_data = raw_data - repmat(mean(raw_data),dataSize,1);
+    % Normalize
+    processed_data = processed_data * sqrt(diag(1./diag(processed_data'*processed_data)));
+    processed_data(isnan(processed_data)==1)=0;
+end
